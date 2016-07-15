0dddf9e5657e0e2756ecda24df05fc79f927b134
逼格波斯 <1019510674@qq.com>
2016-07-15 17:46:18 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="footer" align="center">
+    <p>&copy; <?php echo $site_name."  ".date('Y'); ?>  Power By <a href="http://weibo.com/devillaws">BIGBOSSweibo
+	</a>thx for <a href="https://github.com/orvice/ss-panel">orvice</a></p>
+</div>
---
+++ nav.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <ul class="nav nav-pills pull-right" role="tablist">
+        <li role="presentation"><a href="index.php">主页</a></li>
+        <li role="presentation"><a href="user/login.php">登录</a></li>
+        <li role="presentation"><a href="user/reg.php">注册</a></li>
+		<li role="presentation"><a href="http://www.dmm.com">大咪咪</a></li>
+    </ul>
+    <!-- <h3 class="text-muted"><?php echo $site_name; ?></h3> -->
+	  <img src="/css/banner.png" alt="Free Shadowsocks by zmrbk.com"  width="250px" height="50px" />
+</div>
